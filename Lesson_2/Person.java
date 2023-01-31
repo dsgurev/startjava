2e810284c412820d1b2587a6011242c786e48897
dsgurev <ds.gurev@gmail.com>
2023-01-31 23:12:34 +0500

    new file:   Lesson_2/Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String gender = "male";
+    String name = "Arnold";
+    Float height = 1.88f;
+    Float weight = 115.21f;
+    int age = 21;
+
+    void walk(){
+        System.out.println("walking");
+    }
+
+    void sit(){
+        System.out.println("sitting");
+    }
+
+    void run(){
+        System.out.println("running");
+    }
+
+    void speak(){
+        System.out.println("speaking");
+    }
+
+    void learnJava(){
+        System.out.println("study");
+    }
+}

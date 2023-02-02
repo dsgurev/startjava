b12d1d5b98fa876012992ac4c49b4ca047042bb7
dsgurev <ds.gurev@gmail.com>
2023-02-02 23:09:58 +0500

    get/set 	modified:   Lesson_2/Wolf.java 	modified:   Lesson_2/WolfTest.java

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,17 +2,17 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf bigBadWolf = new Wolf();
-        bigBadWolf.gender = "самец";
-        bigBadWolf.nickname = "Серый Волк";
-        bigBadWolf.weight = 50.5f;
-        bigBadWolf.age = 5;
-        bigBadWolf.color = "серый";
+        bigBadWolf.setGender("самец");
+        bigBadWolf.setNickname("Серый Волк");
+        bigBadWolf.setWeight(50.5f);
+        bigBadWolf.setAge(5);
+        bigBadWolf.setColor("серый");
         System.out.println("Это волк !!!");
-        System.out.println("пол - " + bigBadWolf.gender);
-        System.out.println("кличка - " + bigBadWolf.nickname);
-        System.out.println("вес = " + bigBadWolf.weight);
-        System.out.println("возраст = " + bigBadWolf.age + " лет");
-        System.out.println("окрас - " + bigBadWolf.color);
+        System.out.println("пол - " + bigBadWolf.getGender());
+        System.out.println("кличка - " + bigBadWolf.getNickname());
+        System.out.println("вес = " + bigBadWolf.getWeight());
+        System.out.println("возраст = " + bigBadWolf.getAge() + " лет");
+        System.out.println("окрас - " + bigBadWolf.getColor());
         bigBadWolf.walk();
         bigBadWolf.sit();
         bigBadWolf.run();

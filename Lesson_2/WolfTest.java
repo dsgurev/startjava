d5cf2db84b5c65dc3ff2031143f406ceb8a851d1
dsgurev <ds.gurev@gmail.com>
2023-02-06 22:34:25 +0500

    modified:   Lesson_2/WolfTest.java

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -7,12 +7,14 @@ public class WolfTest {
         bigBadWolf.setWeight(50.5f);
         bigBadWolf.setAge(5);
         bigBadWolf.setColor("серый");
+
         System.out.println("Это волк !!!");
         System.out.println("пол - " + bigBadWolf.getGender());
         System.out.println("кличка - " + bigBadWolf.getNickname());
         System.out.println("вес = " + bigBadWolf.getWeight());
         System.out.println("возраст = " + bigBadWolf.getAge() + " лет");
         System.out.println("окрас - " + bigBadWolf.getColor());
+        
         bigBadWolf.walk();
         bigBadWolf.sit();
         bigBadWolf.run();

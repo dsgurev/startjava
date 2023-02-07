93a6f417f5c9fe9918b502450face4707df20903
dsgurev <ds.gurev@gmail.com>
2023-02-07 22:06:52 +0500

    new file:   Lesson_2/Calculator.java 	new file:   Lesson_2/CalculatorTest.java

---
+++ Lesson_2/Calculator.java
@@ -0,0 +1,49 @@
+public class Calculator {
+
+    private int a;
+    private int b;
+    private char sign;
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public void setSign(char sign) {
+        this.sign = sign;
+    }
+
+    public void calc() {
+        int result = 0;
+        switch (sign) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+            case '^':
+                result = 1;
+                for (int i = 0; i < b; i++) {
+                    result *= a;
+                }
+                break;
+            case '%':
+                result = a % b;
+                break;
+            default:
+                System.out.println("Операция не поддерживается");
+                return;
+        }
+        System.out.println(a + " " + sign + " " + b + " = " + result);
+    }
+}
---
+++ Lesson_2/CalculatorTest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+        String answer = "yes";
+        Calculator calculator = new Calculator();
+        Scanner scan = new Scanner(System.in);
+
+        while (answer.equals("yes")) {
+            System.out.print("Введите первое число: ");
+            calculator.setA(scan.nextInt());
+            System.out.print("Введите знак математической операции: ");
+            calculator.setSign(scan.next().charAt(0));
+            System.out.print("Введите второе число: ");
+            calculator.setB(scan.nextInt());
+            calculator.calc();
+            do {
+                System.out.print("Хотите продолжить вычисления? [yes/no]: ");
+                answer = scan.next();
+                if (answer.equals("no")) {
+                    return;
+                }
+            } while (!answer.equals("yes"));
+        }
+    }
+}
